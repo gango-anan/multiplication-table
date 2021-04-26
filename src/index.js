e4358d8b87dff5371d682ce6b67d32319d3fa7c9
Galiwango Ananiya <galiwango101@gmail.com>
2021-04-26 23:05:54 +0300

    Add mouseover and mouseout event handlers.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+// Demonstrating event propagation
+
 import './styles.css';
+
+// find table
+const table = document.getElementById("multiplication");
+const tcols = table.getElementsByTagName("col");
+
+// delegate event handler
+table.addEventListener( "mouseover", TableHandler );
+table.addEventListener( "mouseout", TableHandler );
+
+// event handler
+function TableHandler(e) {
+
+	let t = e.target;
+	if (t.nodeName != "TD") return;
+	
+	// let cName = ( e.type == "mouseover" ? "active" : "");
+	// t.parentNode.className = cName;
+	// tcols[ t.cellIndex ].className = cName;
+
+	t.parentNode.className = tcols[ t.cellIndex ].className =
+	( e.type == "mouseover" ? "active" : "");
+	
+}
